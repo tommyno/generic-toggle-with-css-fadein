9741190421d8a2c3e9d21cce8eba79d4e30e0f4c
tommyno <tommy@netliferesearch.com>
2016-09-21 15:09:44 +0200

    Add project

---
+++ README.md
@@ -0,0 +1,15 @@
+# Generic toggle with css fadein
+
+Generic JS toggle, using CSS to fade in content.
+
+## How to use
+
+Example:
+```
+<a data-toggle="[data-target='content']" data-text-swap="Hide content">Show content</a>
+<div data-target="content">
+  <p>Hidden content</p>
+</div>
+```
+
+Optional: Use `data-text-swap="New text"` to change text when toggled.
---
+++ generic-toggle.js
@@ -0,0 +1,73 @@
+(function() {
+
+    "use strict";
+
+     // Init
+    addListener(document.querySelectorAll('[data-toggle]'), 'click');
+
+
+    // Generic event listener - used for toggling
+    /**
+    * @name addListener
+    * @desc Attaches event listeners to all matching elements - used for toggling
+    * @attr selector (HTMLelement) - Element to check
+    * @attr event (string) - Event to listen for (ex: 'click')
+    */
+    function addListener(selector, event) {
+        for (var i = 0; i < selector.length; i++) {
+
+            // Click event
+            if (event === 'click') {
+                selector[i].addEventListener('click', _clickHandler);
+            }
+
+            // Other event
+            // ...
+        }
+    }
+
+
+    // Click handler
+    /**
+    * @name _clickHandler
+    * @desc Toggles other element on click
+    * @attr el (HTMLelement) - Element it listens to
+    */
+    function _clickHandler(event) {
+        var el = event.currentTarget;
+        // Extract target from data-attr
+        var targetAttr = el.getAttribute('data-toggle');
+        // Toggle target
+        var target = document.querySelector(targetAttr);
+        target.classList.toggle("is-visible");
+
+        // Change text
+        _swapText(el);
+    }
+
+
+
+    // Swap text - used on toggling
+    /**
+    * @name _swapText
+    * @desc Adds original text as an data attr to element, then swaps between old and new text
+    * @attr el (HTMLelement) - Element to check
+    */
+    function _swapText(el) {
+
+        var swapText = el.getAttribute('data-text-swap');
+
+        if (swapText) {
+            // Save original text as data attr
+            if (!el.getAttribute('data-text-original')) {
+                el.setAttribute('data-text-original', el.innerHTML);
+            }
+
+            var originalText = el.getAttribute('data-text-original');
+            el.innerHTML = (el.innerHTML === originalText ? swapText : originalText);
+        }
+
+    }
+
+
+})();
